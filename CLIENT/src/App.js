247360db00284e2e83eba1db76002da71e505c50
Carsefer <107973841+Carsefer@users.noreply.github.com>
2022-10-11 18:38:13 -0500

    landing finish

--- CLIENT/src/App.js
+++ CLIENT/src/App.js
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
 import LandingHome from "./components/LandingHome/LandingHome";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingHome />} />
-        <Route path="*" element={<Footer />} />
       </Routes>
     </>
   );
---
+++ CLIENT/src/components/LandingHome/LandingHome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../Footer/Footer";
+export default function LandingHome() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to Express Clothes</h1>
+        <button>Enter to Shop</button>
+        <button>Create a Store</button>
+      </div>
+      <Footer />
+    </>
+  );
+}
