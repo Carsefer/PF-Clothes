6411953c66397655aac5712b58c361a19907068f
Carsefer <107973841+Carsefer@users.noreply.github.com>
2022-10-11 19:10:35 -0500

    Update LandingHome.jsx

--- CLIENT/src/components/LandingHome/LandingHome.jsx
+++ CLIENT/src/components/LandingHome/LandingHome.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Footer from "../Footer/Footer";
 export default function LandingHome() {
   return (
-    <>
-      <div>
-        <h1>Welcome to Express Clothes</h1>
-        <button>Enter to Shop</button>
-        <button>Create a Store</button>
+    <div>
+      <div className="container">
+        <h1 className="">Welcome to Express Clothes</h1>
+        <h4>Buy or increase your store selling Clothes</h4>
+        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+          <button className="btn btn-warning me-md-2 btn-lg">
+            Enter to Shop
+          </button>
+          <button className="btn btn-warning me-md-2 btn-lg">
+            Create a Store
+          </button>
+        </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
