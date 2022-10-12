8e99b9eda9e5a2d4592bafcbe882f39bc5079cd8
Carsefer <107973841+Carsefer@users.noreply.github.com>
2022-10-11 19:13:51 -0500

    LadingHome

--- CLIENT/src/components/LandingHome/LandingHome.jsx
+++ CLIENT/src/components/LandingHome/LandingHome.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Footer from "../Footer/Footer";
+import { Link } from "react-router-dom";
 export default function LandingHome() {
   return (
     <div>
@@ -7,12 +8,16 @@ export default function LandingHome() {
         <h1 className="">Welcome to Express Clothes</h1>
         <h4>Buy or increase your store selling Clothes</h4>
         <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button className="btn btn-warning me-md-2 btn-lg">
-            Enter to Shop
-          </button>
-          <button className="btn btn-warning me-md-2 btn-lg">
-            Create a Store
-          </button>
+          <Link to="/home">
+            <button className="btn btn-warning me-md-2 btn-lg">
+              Enter to Shop
+            </button>
+          </Link>
+          <Link to="/register">
+            <button className="btn btn-warning me-md-2 btn-lg">
+              Create a Store
+            </button>
+          </Link>
         </div>
       </div>
       <Footer />
