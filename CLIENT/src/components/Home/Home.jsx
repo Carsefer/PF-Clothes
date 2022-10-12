42b8dfb5ffb11811b00ba026db97abd0dcdfbd2c
JuanM <juanmanesevich25@gmail.com>
2022-10-12 09:04:15 -0300

    Home trae todos los productos y los renderiza en una card cada uno

--- CLIENT/src/components/Home/Home.jsx
+++ CLIENT/src/components/Home/Home.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 import "./Home.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../../redux/actions";
+import Card from "../Card/Card";
 
 export default function Home() {
+
+    const dispatch = useDispatch();
+    const allProducts = useSelector(state => state.products);
+
+    useEffect(() => {
+        dispatch(getProducts());
+    }, [dispatch]);
+
+    return(
+        <div className="home">
+            <h1>Home</h1>
+            <div className="productos">
+                {allProducts?.map(p =>
+                    <Card
+                    key = {p.id}
+                    id = {p.id}
+                    img = {p.img}
+                    title = {p.title}
+                    price = {p.price}
+                    />
+                    )}
+            </div>
+        </div>
+    )
 
 }
